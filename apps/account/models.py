a064d758b1739a0c63b64b5fce9bb5745cd2da00
aybike <aybike.d44@gmail.com>
2022-06-21 23:44:25 +0600

    last

--- apps/account/models.py
+++ apps/account/models.py
@@ -4,7 +4,7 @@ from django.core.mail import send_mail
 from decouple import config
 
 class CustomUserManager(BaseUserManager):
-    def _create(self, email, password, **extra_fields):
+    def _create(self, email, password, name=None, last_name=None, **extra_fields):
         if not email:
             raise ValueError('Email cannot be empty')
         user = self.model(email=email, **extra_fields)
@@ -12,27 +12,27 @@ class CustomUserManager(BaseUserManager):
         user.save()
         return user
 
-    def create_user(self, email, password, **extra_fields):
+    def create_user(self, email, password, name=None, last_name=None, **extra_fields):
         extra_fields.setdefault('is_active', False)
         extra_fields.setdefault('is_staff', False)
-        return self._create(email, password, **extra_fields)
+        return self._create(email, password, name=None, last_name=None, **extra_fields)
 
-    def create_superuser(self, email, password, **extra_fields):
+    def create_superuser(self, email, password, name=None, last_name=None, **extra_fields):
         extra_fields.setdefault('is_active', True)
         extra_fields.setdefault('is_staff', True)
-        return self._create(email, password, **extra_fields)
+        return self._create(email, password, name=None, last_name=None, **extra_fields)
 
 class CustomUser(AbstractBaseUser):
     email=models.EmailField(unique=True)
-    # name=models.CharField(max_length=50)
-    # last_name=models.CharField(max_length=150, blank=True, null=True)
+    name=models.CharField(max_length=50, blank=True, null=True)
+    last_name=models.CharField(max_length=150, blank=True, null=True)
     is_active=models.BooleanField(default=False)
     is_staff=models.BooleanField(default=False)
     activation_code=models.CharField(max_length=8, blank=True, null=True)
 
     objects=CustomUserManager()
     USERNAME_FIELD='email'
-    # REQUIRED_FIELDS=['name', 'last_name']
+    REQUIRED_FIELDS=['name', 'last_name']
 
 
     def __str__(self):
