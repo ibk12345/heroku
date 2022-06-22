5054c058cf292cf92cd3d921e51c104117750059
aybike <aybike.d44@gmail.com>
2022-06-22 21:39:03 +0600

    media

--- config/settings.py
+++ config/settings.py
@@ -167,6 +167,9 @@ STATICFILES_FINDERS = (
 )
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR / 'media')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
